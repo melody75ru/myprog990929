0cf1472b39b6b67cbce7475a2128a5b8c64398ac
unknown <>
2020-12-21 14:02:54 +0330

    Delete News component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AboutComponent } from './Site/about/about.component';
+import { ProductComponent } from './Site/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+        AboutComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
